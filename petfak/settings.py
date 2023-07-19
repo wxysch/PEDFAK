3cf32b893d0f4edafa11297ce6dbb04900b3550e
alinu1 <uwu1343@gmail.com>
2023-07-19 15:39:35 +0600

    first models

--- petfak/settings.py
+++ petfak/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'petfak.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,6 +105,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Settings(models.Model):
+    title = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='logo')
+    contacts = models.TextField()
+    press_service = models.TextField()
+    selection_committee = models.TextField()
+    vk = models.URLField(null=True,blank=True)
+    twitter = models.URLField(null=True,blank=True)
+    instagram = models.URLField(null=True,blank=True)
+    facebook = models.URLField(null=True,blank=True)
+    youtube = models.URLField(null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Настройка'
+        verbose_name_plural = 'Настройки'
